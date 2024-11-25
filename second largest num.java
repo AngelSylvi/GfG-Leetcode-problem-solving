f071d67b766099b6e6b5bfd22522112db7f866f5
AngelSylvi <angelinesylvia2005@gmail.com>
2024-11-25 23:54:46 +0530

    first commit

---
+++ second largest num.java
@@ -0,0 +1,21 @@
+//Driver code
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        int n=arr.length;
+        int first=-1, second=-1;
+        for (int i=0;i<n;i++){
+            if(arr[i]>first){
+                second=first;
+                first=arr[i];
+            }
+            else if(arr[i]<first && arr[i]>second){
+                 second=arr[i];
+                 
+            }
+         
+        }
+        return second; 
+    }
+}
+
